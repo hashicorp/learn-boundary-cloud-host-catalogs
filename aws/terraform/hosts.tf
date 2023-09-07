2cce564fa99424361f22fc2ab17109e4a1b642e0
stellarsquall <stellarsquall@protonmail.ch>
2023-09-07 16:40:53 -0600

    adds aws_availability_zone, aws_vpc, and aws_subnet to avoid using default VPC

--- aws/terraform/hosts.tf
+++ aws/terraform/hosts.tf
@@ -1,7 +1,47 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-# Configure the AWS hosts
+# Configure the AWS VM hosts
+
+data "aws_availability_zones" "boundary" {}
+
+resource "aws_vpc" "boundary_hosts_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "boundary_hosts_vpc"
+  }
+}
+
+resource "aws_subnet" "boundary_hosts_subnet" {
+  vpc_id                  = aws_vpc.boundary_hosts_vpc.id
+  cidr_block              = aws_vpc.boundary_hosts_vpc.cidr_block
+  availability_zone       = data.aws_availability_zones.boundary.names[0]
+  map_public_ip_on_launch = true
+  
+  tags = {
+    Name = "boundary_hosts_subnet"
+  }
+}
+
+resource "aws_security_group" "boundary_ssh" {
+  name        = "boundary_allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.boundary_hosts_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
 variable "instances" {
   default = [
     "boundary-1-dev", 
@@ -20,28 +60,11 @@ variable "vm_tags" {
   ]
 }
 
-resource "aws_security_group" "boundary-ssh" {
-  name        = "boundary_allow_ssh"
-  description = "Allow SSH inbound traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_ssh"
-  }
-}
-
-resource "aws_instance" "boundary-instance" {
+resource "aws_instance" "boundary_instance" {
   count                  = length(var.instances)
   ami                    = "ami-083602cee93914c0c"
   instance_type          = "t3.micro"
-  availability_zone      = "us-east-1a"
-  security_groups        = ["boundary_allow_ssh"]
-  vpc_security_group_ids = ["${aws_security_group.boundary-ssh.id}"]
+  subnet_id              = aws_subnet.boundary_hosts_subnet.id
+  vpc_security_group_ids = [aws_security_group.boundary_ssh.id]
   tags                   = var.vm_tags[count.index]
 }
